f537e0953d18f181d230d8e76020d53bb8af963d
SSY1203 <tlstjsdud566@naver.com>
2025-07-19 22:38:07 +0900

    구글 애널리틱스 태그 관리자 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Navigation } from "@/components/navigation";
 import { Footer } from "@/components/footer";
 import Image from "next/image";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" suppressHydrationWarning>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PM69JFQB');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
@@ -42,6 +54,12 @@ export default function RootLayout({
         <Navigation />
         <main className="fade-in relative z-10 flex-grow">{children}</main>
         <Footer />
+        <Script id="google-tag-manager-noscript" strategy="afterInteractive">
+          {`
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PM69JFQB"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          `}
+        </Script>
       </body>
     </html>
   );
